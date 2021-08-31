397722e8e89d52aa8ac5233e11307ebb59fe2804
Nick <nlarsen@smu.edu>
2021-08-31 18:21:58 -0500

    Initial Commit

---
+++ SinkOrSwim7323/SinkOrSwim7323App.swift
@@ -0,0 +1,17 @@
+//
+//  SinkOrSwim7323App.swift
+//  SinkOrSwim7323
+//
+//  Created by Nicholas Larsen on 8/31/21.
+//
+
+import SwiftUI
+
+@main
+struct SinkOrSwim7323App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
