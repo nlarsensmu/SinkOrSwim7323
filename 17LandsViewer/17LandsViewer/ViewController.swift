2f3a91a9142a7e9b1e04a3075ac089c4e46352bc
Nick <nlarsen@smu.edu>
2021-08-31 19:25:01 -0500

    Add Hello World.

---
+++ 17LandsViewer/17LandsViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  17LandsViewer
+//
+//  Created by Nicholas Larsen on 8/31/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Hello World")
+    }
+
+
+}
+
