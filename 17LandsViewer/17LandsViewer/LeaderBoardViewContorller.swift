0b32302bebc79749041768c6a5c65b342e6e4a57
Nick <nlarsen@smu.edu>
2021-09-03 17:48:41 -0500

    Add LeaderBoard classes

---
+++ 17LandsViewer/17LandsViewer/LeaderBoardViewContorller.swift
@@ -0,0 +1,32 @@
+//
+//  LeaderBoardViewContorller.swift
+//  17LandsViewer
+//
+//  Created by Nicholas Larsen on 9/3/21.
+//
+
+import UIKit
+
+class LeaderBoardViewContorller: UIViewController, UITextFieldDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func formatText(_ sender: UITextField) {
+    }
+    
+	
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- 17LandsViewer/17LandsViewer/MagicMetadataModel.swift
+++ 17LandsViewer/17LandsViewer/MagicMetadataModel.swift
@@ -8,5 +8,48 @@
 import UIKit
 
 class MagicMetadataModel: NSObject {
+    
+    private static var _sharedInstance:MagicMetadataModel? = {
+        var sharedInstance:MagicMetadataModel? = nil
+        
+        if(_sharedInstance == nil) {
+            _sharedInstance = MagicMetadataModel()
+        }
+        
+        return sharedInstance
+    }()
+    
+    
+    
+    func getLeaderBoards(expansion: String, format: String) -> PlayerLeaderBoard? {
+        let url : String = "https://www.17lands.com/data/leaderboard?expansion=\(expansion)&format=\(format)"
+        var request = URLRequest(url: URL(string: url)!)
+        request.httpMethod = "GET"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        let session = URLSession.shared
+        
+        var dataRecieved: Data?
+        let sem = DispatchSemaphore.init(value: 0)
+        
+        var leaderBoard: NSDictionary? = nil
+        let task = session.dataTask(with: request) { data, response, error in
+            defer { sem.signal() }
+            
+            if let error = error {
+                print("error getting leaderboard \(error)")
+                return
+            }
+            dataRecieved = data
+        }
+        task.resume()
+        sem.wait()
+        do{
+            leaderBoard = try JSONSerialization.jsonObject(with: dataRecieved!) as? NSDictionary
+        }
+        catch{
+            print("error getting leaderboard \(error)")
+        }
+        return PlayerLeaderBoard(leaderBoard: leaderBoard!)
+    }
 
 }
